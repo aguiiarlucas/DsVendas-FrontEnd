2d1a22d13e2759ae6e014585a5493cb57d957053
aguiiarlucas <lucascostaaguiar.com>
2021-05-05 22:01:55 -0300

    Static charts

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,34 +1,17 @@
 import Chart from 'react-apexcharts'
 
-function BarChart() {
+const BarChart = () => {
     const options = {
+        plotOptions: { bar: { horizontal: true, } },
 
-        plotOptions: {
-            bar: {
-                horizontal: true,
-            }
-        },
-    };
+    }; const mockData = {
+        labels: { categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé'] },
+        series: [{ name: "% Sucesso", data: [43.6, 67.1, 67.7, 45.6, 71.1] }]
 
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]
-            }
-        ]
-    };
-    return (
+    }; return (
         <Chart
-            option={{ ...options, xaxis: mockData.labels }}
-            series={mockData.series}
-            type="bar"
-            height="240"
-        />
-    );
+            options={{ ...options, xaxis: mockData.labels }} series={mockData.series}
+            type="bar" height="240" />);
 }
 
 export default BarChart;
