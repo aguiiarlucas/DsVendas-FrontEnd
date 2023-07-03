c767705873c7fedcc38b309abc5ae6ffa63a5edc
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2023-07-03 15:11:24 -0300

    Back-end Deleted

--- backend/src/main/java/com/devsuperior/sdvendas/repositories/SellerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.sdvendas.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devsuperior.sdvendas.entities.Seller;
-
-public interface SellerRepository extends JpaRepository<Seller, Long> {
-
-}
